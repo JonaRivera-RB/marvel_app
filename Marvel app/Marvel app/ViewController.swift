2cb7eb132170e48f9b90bf059d8cc918e1b1b53e
Jonathan Rivera <misael@MacBook-Air-de-Misael-5.local>
2019-11-24 19:19:35 -0600

    add main files

---
+++ Marvel app/Marvel app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Marvel app
+//
+//  Created by Misael Rivera on 24/11/19.
+//  Copyright © 2019 Misael Rivera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
